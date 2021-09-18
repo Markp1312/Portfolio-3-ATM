5243f33fb11cee2aed160419d65420873ad9268d
Markp1312 <mpg.peeters3@gmail.com>
2021-09-18 10:29:23 +0000

    created welcome function

--- run.py
+++ run.py
@@ -15,10 +15,20 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('ATM_Machine')
 
 
+def welcome():
+    x = datetime.datetime.now()
+    print(x)
+    print("Welcome to Mark Financial Services")
+    print("Please make one of the following options")
+    
+
 """
 Run all the program functions
 
 """
 
 def main():
+    welcome()
+    choice()
+
 main()
